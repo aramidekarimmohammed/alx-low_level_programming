cca4db2890e8e8195ab747a9b19c451acc2c0e5d
aramide <aramidekarimmohammed@gmail.com>
2021-09-14 12:17:50 -0700

    memcpy

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,39 +1,20 @@
 #include "holberton.h"
 
 /**
- * _memset - fill n bytes of memory with a constant value
- * @s: pointer to memory area
- * @b: constant value
- * @n: number of bytes to fill
- * Return: memory area
+ * _memset - fills memory with a constant byte
+ * @s: memory space to be filled
+ * @b: byte to fill with
+ * @n: number of spaces to fill
+ *
+ * Return: pointer to memory area s
  */
-
 char *_memset(char *s, char b, unsigned int n)
 {
+	unsigned int i;
 
-	int size = n; /* only accept positive sizes */
-
-	if (size > 0)
+	for (i = 0; i < n; i++)
 	{
-		int i;
-
-		for (i = 0; i < size; i++)
-			s[i] = b;
-	}
-
-	return (s);
-}
-
-/* pointer arithmetic version
-char *_memset(char *s, char b, unsigned int n)
-{
-	int size = n;
-	if (size > 0)
-	{
-		int i;
-		for (i = 0; i < size; i++)
-		     *(s + i) = b;
+		*(s + i) = b;
 	}
 	return (s);
 }
-*/
